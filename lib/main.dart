56283235fdc716e4cea73026b3e7c69de075c21e
Maulanaputra07 <maulanaaputraaa077@gmail.com>
2025-07-30 20:55:42 +0700

    splash screen & icon app

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,12 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:flutter_native_splash/flutter_native_splash.dart';
 
 void main() async {
+  WidgetsBinding widgetsBinding = WidgetsFlutterBinding.ensureInitialized();
+  FlutterNativeSplash.preserve(widgetsBinding: widgetsBinding);
+
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
@@ -19,8 +23,45 @@ void main() async {
     );
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget{
   const MyApp ({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+  // @override
+  // void initState(){
+  //   super.inistState();
+  // }
+
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     debugShowCheckedModeBanner: false,
+  //     theme: ThemeData(
+  //       scaffoldBackgroundColor: Color(0xFFFAFAFA)
+  //     ),
+  //     home: const HomePage(),
+  //     routes: {
+  //       '/addtask': (context) => const AddTaskPage(),
+  //     },
+  //   );
+  // }
+}
+
+
+class _MyAppState extends State<MyApp>{
+  @override
+  void initState(){
+    super.initState();
+    initialization();
+  }
+
+  void initialization() async {
+    print("Pausing...");
+    await Future.delayed(const Duration(seconds: 3));
+    print("Unpausing");
+    FlutterNativeSplash.remove();
+  }
 
   @override
   Widget build(BuildContext context) {
